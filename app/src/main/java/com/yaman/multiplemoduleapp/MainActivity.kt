179463a0e26207088a1e2fb9904d89120883081a
Yaman Aswal <yaman@appsquad.com>
2024-04-21 09:19:19 +0530

    Code Updated

--- app/src/main/java/com/yaman/multiplemoduleapp/MainActivity.kt
+++ app/src/main/java/com/yaman/multiplemoduleapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yaman.multiplemoduleapp.databinding.TextItemBinding
 import com.yaman.pdf_viewer.ui.PdfViewerActivity
+import com.yaman.recycler_view.generic_recycler_views.core.adapters.GenericAdapter
 import com.yaman.recycler_view.generic_recycler_views.core.adapters.GenericAdapterPagination
 import com.yaman.recycler_view.generic_recycler_views.core.view_holders.BaseViewHolder
 import com.yaman.runtime_permissions.coroutine_permissions.PermissionManager
@@ -22,7 +23,8 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: GenericAdapterPagination<String>
+    private lateinit var genericAdapterPagination: GenericAdapterPagination<String>
+    private lateinit var genericAdapter: GenericAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +33,38 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
 //            demoPdfViewer()
 //            demoRuntimePermissions()
-            demoRecyclerViewLongList()
+//            demoRecyclerViewLongList()
+            demoRecyclerView()
         }
+    }
+
+    private fun demoRecyclerView() {
+        val rv = findViewById<RecyclerView>(R.id.recyclerView)
+        rv.visibility = View.VISIBLE
+
+        val demoArr = ArrayList<String>()
+
+        for (i in 1..100) {
+            demoArr.add("Date ${i}")
+        }
+
+        Log.e("TAG", "demoRecyclerViewLongList: $demoArr")
+
+        val layoutManager = LinearLayoutManager(this) // or GridLayoutManager(this, spanCount)
+        rv.layoutManager = layoutManager
+
+        genericAdapter = object : GenericAdapter<String>(R.layout.text_item){
+            override fun onBindViewHold(holder: BaseViewHolder<String>, position: Int) {
+                val binding = holder.binding as TextItemBinding
+                binding.textId.text = getItem(position)
+                Log.e("TAG", "onBindViewHold: $position - ${getItem(position)}")
+            }
+
+        }
+
+        rv.adapter = genericAdapter
+
+        genericAdapter.updateList(demoArr)
     }
 
     private fun demoRuntimePermissions() {
@@ -66,10 +98,10 @@ class MainActivity : AppCompatActivity() {
         rv.layoutManager = layoutManager
 
         val pageSize = 50 // Number of items per page
-        adapter = object : GenericAdapterPagination<String>(
+        genericAdapterPagination = object : GenericAdapterPagination<String>(
             R.layout.text_item, {
                 // Load more data
-                adapter.addMoreList(demoArr, pageSize, it)
+                genericAdapterPagination.addMoreList(demoArr, pageSize, it)
                 Log.e("TAG", "demoRecyclerViewLongList: addMoreList called")
             }) {
 
@@ -80,12 +112,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        rv.adapter = adapter
+        rv.adapter = genericAdapterPagination
 
-        adapter.setRecyclerViewScrollListener(rv)
+        genericAdapterPagination.setRecyclerViewScrollListener(rv)
 
         // Load initial data (first page)
-        adapter.updateInitialData(demoArr, pageSize)
+        genericAdapterPagination.updateInitialData(demoArr, pageSize)
     }
 
 }
